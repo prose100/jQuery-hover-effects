b0304e6021e6065057c48e537bf0f70e3ddf82c6
Paul Rose <prose100@hotmail.com>
2016-01-05 13:09:23 -0600

    Created a slideDiagonalDownLeft option.

--- src/js/hovereffect.js
+++ src/js/hovereffect.js
@@ -171,6 +171,41 @@
 		)
 	}
 
+	HoverEffect.prototype.slideDiagonalDownLeft = function(overlay, height, width) {
+		
+		var _this = this;
+
+		$(this.element).hover(function() {	
+			overlay.stop()
+				.css({
+					left: width,
+					top: 0,
+					opacity: _this.config.opacity,
+					display: "block"
+				})
+				.animate({
+					left: 0,
+					top: 0,
+					height: height,
+					width: width
+				}, _this.config.duration)}, function() {	
+			overlay.stop()
+				.animate({
+					left: width,
+					top: 0,
+					height: "0px",
+					width: "0px"
+				}, _this.config.duration, function(){
+				overlay
+					.css({
+						opacity: 0,
+						display: "none"
+					})
+				})	
+			}
+		)
+	}
+
 	HoverEffect.prototype.fadeInAndOut = function(overlay, height, width) {
 		
 		var _this = this;
