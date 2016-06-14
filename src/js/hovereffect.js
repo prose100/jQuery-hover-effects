074799bd03eb5a4e3aaed0ce88faa583b8a3c29e
Paul Rose <prose100@hotmail.com>
2016-06-14 12:42:32 -0500

    Add comments. Simplify html.

--- src/js/hovereffect.js
+++ src/js/hovereffect.js
@@ -9,11 +9,10 @@
 		wrapper_class: 'wrapper',
 		hover_class: 'hover',
 		base_class: 'base',
-		elementWidth: '0px',
-        elementHeight: '0px',
+		elementWidth: '100px',
+        elementHeight: '100px',
 		overlayWidth: '0px',
-		overlayHeight: '0px',
-		zindex: 2
+		overlayHeight: '0px'
 	};
 
 	function HoverEffect(element, options) {
@@ -26,31 +25,35 @@
 
 	HoverEffect.prototype.init = function() {
 
+		//create divs with properties
 		var dimensions = this.getDimensions();
-		$this.css({'width':dimensions.cWidth, 'height':dimensions.cHeight});
-
 		var divs = this.createDivs(dimensions);
 		this.setProperties(divs, dimensions);
 
 		if (settings.slidePath[0]=='') {
+			//no slidePath, so just fadeIn and fadeOUt
 			divs.hover.css(this.getCoordinates('zero', dimensions));
 			divs.hover.css({'display':'none'});
 			this.fade(divs);
 		} else {
+			//with a slidePath, animate accordingly
 			var nodes = this.getNodes(dimensions);
 			this.goToStart(nodes, divs);
 			this.animate(nodes, divs);
 		}
 	};
 
+	/* ---------------------------------------------
+		Content
+	   --------------------------------------------- */
+	
+	//returns element's and overlay's dimensions
 	HoverEffect.prototype.getDimensions = function() {
 		var elementWidth = parseInt(settings.elementWidth, 10);
 		var elementHeight = parseInt(settings.elementHeight, 10);
 		var overlayWidth = parseInt(settings.overlayWidth, 10);
 		var overlayHeight = parseInt(settings.overlayHeight, 10);
 
-		elementWidth = elementWidth > 0 ? elementWidth : $this.width();
-		elementHeight = elementHeight > 0 ? elementHeight : $this.height();
 		overlayWidth = overlayWidth > 0 ? overlayWidth : elementWidth;
 		overlayHeight = overlayHeight > 0 ? overlayHeight : elementHeight;
 
@@ -62,31 +65,30 @@
 				}
 	}
 
+	//returns $wrapper (holds the element and $hover) and $hover (holds the overlay)
 	HoverEffect.prototype.createDivs = function(dimensions) {
 
+		//hide overlay
 		var $info = $this.next();
 		$info.hide();
 
 		var $wrapper = $('<div>').addClass('wrapper').addClass(settings.wrapper_class)
 			.css({ 'width':dimensions.cWidth, 'height':dimensions.cHeight, 'position':'relative', 'overflow':'hidden' })
 			.insertAfter($this);
-		
-		var $base = $('<div>').addClass(settings.base_class)
-			.css({ 'width':dimensions.cWidth, 'height':dimensions.cHeight, 'position':'absolute', 'z-index':settings.zindex })
-			.appendTo($wrapper);
 
+		//clone the element, append to the newly created $wrapper, and hide the original
 		$this.clone().appendTo($wrapper);
 		$this.hide();
 		
 		var $hover = $('<div>').addClass(settings.hover_class)
-			.css({'width':dimensions.oWidth, 'height':dimensions.oHeight, 'position':'absolute', 'z-index':settings.zindex-1 })
+			.css({'width':dimensions.oWidth, 'height':dimensions.oHeight, 'position':'absolute'})
 			.appendTo($wrapper);
 
+		//clone	the overlay, show, and append to $hover
 		$info.clone().show().appendTo($hover);
 		
 		var divs = {
 			wrapper : $wrapper,
-			base    : $base,
 			hover   : $hover
 		}
 
@@ -101,12 +103,18 @@
 		if (settings.background_color) { 
 			divs.hover.css({'background-color': settings.background_color}); 
 			}
-
 		if (settings.font_color) { 
 			divs.hover.css({'color': settings.font_color}); 
 			}
+
+		$this.css({'width': dimensions.cWidth, 'height': dimensions.cHeight});
 	}
 
+	/* ---------------------------------------------
+		Positions
+	   --------------------------------------------- */
+
+	//returns positions/nodes for animation path of overlay
 	HoverEffect.prototype.getNodes = function(dimensions) {
 		var nodes = {
 			begin : this.getCoordinates(settings.slidePath[0], dimensions),
@@ -116,6 +124,7 @@
 		return nodes
 	}
 
+	//returns coordinates of begin, end, and base nodes
 	HoverEffect.prototype.getCoordinates = function(position, dimensions) {
 		switch (position) {
 			case 'left':
@@ -140,7 +149,7 @@
 				} else if ((settings.slidePath[0]) == 'right') {
 					return {top:this.locateOverlay(dimensions, 'height'), left:dimensions.cWidth-dimensions.oWidth};
 				} else if ((settings.slidePath[0]) == 'top') {
-					return {top:0, left:console.log(this.locateOverlay(dimensions, 'width'))};
+					return {top:0, left:this.locateOverlay(dimensions, 'width')};
 				} else if ((settings.slidePath[0]) == 'bottom') {
 					return {top:dimensions.cHeight-dimensions.oHeight, left:this.locateOverlay(dimensions, 'width')};
 				}
@@ -149,12 +158,19 @@
 		}
 	}
 
+	//centers overlay
+		//note: returns element's height and width, when overlay's dimensions are the same as the element
 	HoverEffect.prototype.locateOverlay = function(dimensions, direction) {
 		var locationOverlay = direction == 'height' ? (dimensions.cHeight - dimensions.oHeight)/2 : 
 							   (dimensions.cWidth - dimensions.oWidth)/2;
 		return locationOverlay;
 	}
 
+	/* ---------------------------------------------
+		Animate/Fade
+	   --------------------------------------------- */
+	
+	//no animation, just fadeIn and fadeOut
 	HoverEffect.prototype.fade = function(divs) {
 		divs.wrapper.hover(function() {
 			divs.hover.stop().fadeIn(settings.duration);
@@ -166,7 +182,8 @@
 
 	HoverEffect.prototype.goToStart = function(nodes, divs) {
 
-		divs.hover.css({'z-index':settings.zindex-1, 'display':'none'})
+		//do not display $hover when returning it to its begin position; when it gets there, display it 
+		divs.hover.css({'display':'none'})
 			 .animate(nodes.begin, 0,
 				function() {
 			  		divs.hover.css({'display':'block'})
@@ -174,12 +191,13 @@
 			);
 	}
 	
+	//animates $hover from begin node to base node upon hover over the $wrapper; 
+	//when no longer hover over $wrapper, $hover goes to end node, after which callback ('goToStart') is run
 	HoverEffect.prototype.animate = function(nodes, divs) {
-
 		var _this = this;
 
 		divs.wrapper.hover(function() {
-			divs.hover.stop().css({'z-index':settings.zindex+1}).animate(nodes.base, settings.duration);
+			divs.hover.stop().animate(nodes.base, settings.duration);
 			}, function() {	
 				divs.hover.stop().animate(nodes.end, settings.duration,
 					function() {
